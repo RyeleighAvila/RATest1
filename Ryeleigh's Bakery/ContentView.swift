1feaee0dc54308150b92f313e2c3909525680b07
RyeleighAvila <Ryeleigha@gmail.com>
2024-04-03 05:23:25 -0500

    Initial Commit

---
+++ Ryeleigh's Bakery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ryeleigh's Bakery
+//
+//  Created by Ryeleigh Avila on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ryeleigh's Bakery/Ryeleigh_s_BakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ryeleigh_s_BakeryApp.swift
+//  Ryeleigh's Bakery
+//
+//  Created by Ryeleigh Avila on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Ryeleigh_s_BakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
